25009ca7c715e30265b4b324d846fc9564b7edc8
Muhammed Ali Gedikli <84350258+gedikliali@users.noreply.github.com>
2022-08-06 01:57:39 +0300

    Delete smart-contract-master directory

--- smart-contract-master/contracts/ERC20Basic.sol
+++
@@ -1,8 +0,0 @@
-pragma solidity ^0.4.11;
-
-contract ERC20Basic {
-  uint public totalSupply;
-  function balanceOf(address who) constant returns (uint);
-  function transfer(address to, uint value);
-  event Transfer(address indexed from, address indexed to, uint value);
-}
